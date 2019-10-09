bfa97e38434f4532ca57e41e0d95a5d1c0dff7c9
Nelson Treméa <whiteaxefun@gmail.com>
2019-10-08 21:57:33 -0400

    Muitas coisas

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -1,5 +1,60 @@
+const conexao = require('../config/conexao')
+
 exports.listar = (req, res) => {
-    res.json({
-        "mensagem": "Listando todos os usuários"
+    
+    const query = "select * from tarefas"
+
+    conexao.query(query, (err, rows) => {
+        if(err){
+            res.status(500)
+            res.json({"message": "Erro interno do Servidor"})
+        }else if(rows.length>0){
+            res.status(200)
+            res.json(rows)
+        }else{
+            res.status(404)
+            res.json({"message": "Nenhuma tarefa encontrada"})
+        }
+    })
+}
+
+exports.listarPorId = (req, res) =>{
+
+    const id = req.params.id
+    const query = "select * from tarefas where id = ?"
+
+    conexao.query(query, [id], (err, rows) => {
+        if(err){
+            res.status(500)
+            res.json({"message": "Erro interno do Servidor"})
+        }else if(rows.length>0){
+            res.status(200)
+            res.json(rows)
+        }else{
+            res.status(404)
+            res.json({"message": "Nenhuma tarefa encontrada"})
+        }
+    })
+}
+
+exports.inserir = (req, res) => {
+
+    const tarefa = {}
+    tarefa.descricao = req.body.descricao
+    tarefa.data = req.body.data
+    tarefa.realizado = req.body.realizado
+    tarefa.categoria_id = req.body.categoria_id
+
+    const query = "insert into tarefas (descricao, data, realizado, categoria_id) values (?, ?, ?, ?)"
+
+    conexao.query(query, [tarefa.descricao, tarefa.data, tarefa.realizado, tarefa.categoria_id], (err, rows) => {
+        if(err){
+            res.status(500)
+            res.json({"message": "Erro interno do Servidor"})
+            console.log(err)
+        }else{
+            res.status(201)
+            res.json({"message": "Tarefa criada com sucesso", "id": rows.insertId})
+        }
     })
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const morgan = require('morgan')
+const YAML = require('yamljs')
+const swaggerUI = require('swagger-ui-express')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+// Logs
 app.use(morgan('combined'))
 
+// Documentação da API
+const swaggerDocument = YAML.load('./docs/swagger.yml')
+app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+// Rotas
 const tarefaRoute = require('./routes/tarefaRoute')
 app.use('/api/v1/tarefas', tarefaRoute)
 
+// Porta da aplicação
 const port = process.env.PORT
 
 app.listen(port, () =>{
